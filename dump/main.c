3d300bfe55918bf6fd79c566a2944a5e4bc18d54
mpiglet <mpiglet@outlook.com>
2021-09-11 15:58:58 +0800

    superblock info

--- dump/main.c
+++ dump/main.c
@@ -16,12 +16,6 @@
 #include "erofs/print.h"
 #include "erofs/io.h"
 
-struct dumpcfg {
-	bool print_superblock;
-	bool print_version; 
-};
-static struct dumpcfg dumpcfg;
-
 static struct option long_options[] = {
 	{"help", no_argument, 0, 1},
 	{0, 0, 0, 0},
@@ -31,7 +25,6 @@ static void usage(void)
 {
 	fputs("usage: [options] erofs-image \n\n"
 	"Dump erofs layout from erofs-image, and [options] are:\n"
-		"	-s		print information about superblock\n"
 		"	-v/-V		print dump.erofs version info\n"
 		"	-h/--help	display this help and exit\n", stderr);
 }
@@ -45,9 +38,6 @@ static int dumpfs_parse_options_cfg(int argc, char **argv)
 	int opt;
 	while((opt = getopt_long(argc, argv, "sSvVi:I:h", long_options, NULL)) != -1) {
 		switch (opt) {
-			case 's':
-				dumpcfg.print_superblock = true;
-				break;
 			case 'v':
 			case 'V':
 				dumpfs_print_version();
@@ -75,39 +65,6 @@ static int dumpfs_parse_options_cfg(int argc, char **argv)
 	return 0;
 }
 
-static void dumpfs_print_superblock()
-{
-	fprintf(stderr, "Filesystem magic number:	0x%04X\n", EROFS_SUPER_MAGIC_V1);
-	fprintf(stderr, "Filesystem blocks: 		%lu\n", sbi.blocks);
-	fprintf(stderr, "Filesystem meta block:		%u\n", sbi.meta_blkaddr);
-	fprintf(stderr, "Filesystem xattr block:	%u\n", sbi.xattr_blkaddr);
-	fprintf(stderr, "Filesystem root nid:		%ld\n", sbi.root_nid);
-	fprintf(stderr, "Filesystem valid inos:		%lu\n", sbi.inos);
-
-	time_t time = sbi.build_time;
-	fprintf(stderr, "Filesystem created:		%s", ctime(&time));
-
-	fprintf(stderr, "Filesystem uuid:		");
-	for (int i = 0; i < 16; i++)
-		fprintf(stderr, "%02x", sbi.uuid[i]);
-	fprintf(stderr, "\n");
-
-	if (erofs_sb_has_lz4_0padding())
-		fprintf(stderr, "Filesystem support lz4 0padding\n");
-	else
-		fprintf(stderr, "Filesystem not support lz4 0padding\n");
-
-	if (erofs_sb_has_big_pcluster())
-		fprintf(stderr, "Filesystem support big pcluster\n");
-	else
-		fprintf(stderr, "Filesystem not support big pcluster\n");
-
-	if (erofs_sb_has_sb_chksum())
-		fprintf(stderr, "Filesystem has super block checksum feature\n");
-	else
-		fprintf(stderr, "Filesystem doesn't have super block checksum feature\n");
-
-}
 
 int main(int argc, char** argv) 
 {
@@ -119,22 +76,6 @@ int main(int argc, char** argv)
 		if (err == -EINVAL)
 			usage();
 		return -1;
-	}
-
-	err = dev_open_ro(cfg.c_img_path);
-	if (err) {
-		erofs_err("open image file failed");
-		return -1;
-	}
-
-	err = erofs_read_superblock();
-	if (err) {
-		erofs_err("read superblock failed");
-		return -1;
-	}	
-
-	if (dumpcfg.print_superblock) {
-		dumpfs_print_superblock();
 	}
 
 	return 0;
